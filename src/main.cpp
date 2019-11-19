8700d61a7d077b90efb93dba957b6ca4ad422fc6
yoav nir <11yoav.nir@gmail.com>
2019-11-19 10:48:17 +0000

    stash commit

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,9 @@ void setup() {
     // pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
     motor_begin();
-    delay(1000);
+    delay(2000);
     set_move_forward_till(true);
+    ultrasound_setup(trigPinFront, echoPinFront);
 }
 
 void loop() {
@@ -26,10 +27,11 @@ void loop() {
         Serial.println("finished turn");
         step = 0;
     }
+
     switch(step){
         case 0:
             if (move_forward_till_on()) {
-              move_forward_till(10, 1.0);
+              move_forward_till(20, 1.0);
             } else {
                 step++;
                 set_move_forward_till(true);
