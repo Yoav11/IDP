74eae9097d8ef1c3a88cbf741bed0643b9b5a894
Ricky <rintaro.kawagishi@gmail.com>
2019-11-21 09:36:32 +0000

    Now we can return to base safely!

--- include/navigation_utils.h
+++ include/navigation_utils.h
@@ -2,7 +2,12 @@
 
 bool move_forward_till_on();
 void set_move_forward_till(bool on);
-void move_forward_till(float desired_d, float speed);
+void move_forward_till(float desired_d, float speed, bool using_front_sensor);
 void move_forward(float speed);
 void change_direction(int final_bearing);
 int detected_mine(int trigPinLeft, int echoPinLeft);
+bool move_to(float x, float y, float speed, bool horizontal_first, bool stopped_turning, int final_bearing);
+bool go_to_safe_zone(float speed, bool horizontal_first, bool stopped_turning);
+bool return_to_base(float speed, bool horizontal_first, bool stopped_turning);
+bool get_to_mine(int distance_up_north, float speed, bool stopped_turning);
+void start_move_to();
--- src/navigation_utils.cpp
+++ src/navigation_utils.cpp
@@ -8,6 +8,11 @@ bool move_forward_till_is_on = false;
 int good_distance_count = 0;
 int good_distances[10];
 
+int move_to_phase = 0;
+bool move_to_is_on = false;
+int get_to_mine_phase = 0;
+bool get_to_mine_is_on = false;
+
 // Returns whether the move_forward_till function should be in use.
 bool move_forward_till_on() {
   return move_forward_till_is_on;
@@ -16,6 +21,23 @@ bool move_forward_till_on() {
 // Set value for whether move_forward_till function should be in use.
 void set_move_forward_till(bool on) {
   move_forward_till_is_on = on;
+}
+
+void start_move_to() {
+  move_to_is_on = true;
+  move_to_phase = 0;
+}
+void stop_move_to() {
+  move_to_is_on = false;
+  move_to_phase = 0;
+}
+void start_get_to_mine() {
+  get_to_mine_is_on = true;
+  get_to_mine_phase = 0;
+}
+void stop_get_to_mine() {
+  get_to_mine_is_on = false;
+  get_to_mine_phase = 0;
 }
 
 // Converts the speed (-1 to 1) to the speed that can be read by the motors (0 to 255). Only the magnitude counts.
@@ -33,11 +55,14 @@ uint16_t motor_2_direction(float speed) {
   return (motor_1_direction(speed) == FORWARD) ? BACKWARD : FORWARD;
 }
 
-// Given speed (-1 to 1) and desired_d (desired distance in cm), this function determines whether it should move and how much power it should give to each motor.
-void move_forward_till(float desired_d, float speed) {
+// Given speed (-1 to 1) and desired_d (desired distance in cm) from whichever sensor we're using to the object (whether using the front sensor or back sensor), this function determines whether it should move and how much power it should give to each motor.
+void move_forward_till(float desired_d, float speed, bool using_front_sensor) {
+  if (!move_forward_till_on()) {return;}
+
+  // NEEDS CHANGE HERE WHEN WE START USING THE BACK SENSOR
   const int distance = get_distance(trigPinFront, echoPinFront);
   const float error = desired_d-distance;
-  const uint16_t motor_1_direc = (distance > desired_d) ? FORWARD : BACKWARD;
+  const uint16_t motor_1_direc = (using_front_sensor ? (distance > desired_d) : (distance < desired_d)) ? FORWARD : BACKWARD;
   const uint16_t motor_2_direc = (motor_1_direc == FORWARD) ? BACKWARD : FORWARD;
 
   Serial.print("distance: ");
@@ -120,16 +145,137 @@ int detected_mine(int trigPinLeft, int echoPinLeft) {
     return -1;
   }
 
-  if (good_distance_count == 5) {
+  if (good_distance_count == 10) {
     sortArray(good_distances, 10);
     good_distance_count = 0;
-<<<<<<< HEAD
     Serial.print("Detected mine at:");
     Serial.println(good_distances[4]);
-=======
->>>>>>> 6f5d6c55e33d8c5060b98bb5a359eaf7e6984ce8
     return good_distances[4];
   }
 
   return -2;
+}
+
+bool move_to(float x, float y, float speed, bool horizontal_first, bool stopped_turning, int final_bearing) {
+  if (!move_to_is_on) {return;}
+  switch (move_to_phase) {
+    case 0:
+      Serial.println("move_to phase 0: face first wall");
+      change_direction(horizontal_first ? 270 : 180);
+      move_to_phase++;
+      break;
+    case 1:
+      if (!stopped_turning) {break;}
+
+      Serial.println("move_to phase 1: switch to the move to first wall phase");
+      move_to_phase++;
+      set_move_forward_till(true);
+      break;
+    case 2:
+      if (stopped_turning) {break;}
+
+      // Serial.println("move_to phase 2: go to first wall");
+      move_forward_till((horizontal_first ? x : y), 1.0, true);
+
+      if (!move_forward_till_on()) { // when the robot has stopped
+        Serial.println("robot has stopped");
+        move_to_phase++;
+      }
+      break;
+    case 3: // Face second wall
+      Serial.println("move_to phase 3: face second wall");
+      change_direction(horizontal_first ? 180 : 270);
+      move_to_phase++;
+      break;
+    case 4: // Change the mode to moving to the west wall phase
+      if (!stopped_turning) {break;}
+
+      Serial.println("move_to phase 4: switch to the move to second wall phase");
+      move_to_phase++;
+      set_move_forward_till(true);
+      break;
+    case 5:
+      if (stopped_turning) {break;}
+
+      Serial.println("go_to_safe_zone phase 2: go to north wall");
+      move_forward_till(horizontal_first ? y : x, 1.0, true);
+
+      if (!move_forward_till_on()) { // when the robot has stopped
+        move_to_phase++;
+      }
+      break;
+    case 6:
+      Serial.println("move_to phase 6: turn to the final bearing");
+      change_direction(final_bearing);
+      move_to_phase++;
+      break;
+    case 7:
+      if (!stopped_turning) {break;}
+      Serial.println("Moved to coordinates:");
+      Serial.print(x, y);
+      stop_move_to();
+      return true;
+      // break;
+  }
+  return false;
+}
+
+bool go_to_safe_zone(float speed, bool horizontal_first, bool stopped_turning) {
+  return move_to(5, 230, speed, horizontal_first, stopped_turning, 270);
+}
+
+bool return_to_base(float speed, bool horizontal_first, bool stopped_turning) {
+  return move_to(20, 20, speed, horizontal_first, stopped_turning, 90);
+}
+
+bool get_to_mine(int distance_up_north, float speed, bool stopped_turning) {
+  if (!get_to_mine_is_on) {return;}
+
+  int duration = 500; // ms
+  switch (get_to_mine_phase) {
+    case 0: // back up a bit
+      Serial.println("get_to_mine phase 0: Backing up a bit");
+      move_forward(-speed);
+      delay(duration);
+      motor_stop();
+      get_to_mine_phase++;
+      break;
+    case 1: // turn north
+      Serial.println("get_to_mine phase 1: turning north");
+      change_direction(0);
+      get_to_mine_phase++;
+      break;
+    case 2: // switch to "move up north"
+      if (!stopped_turning) {break;}
+      Serial.println("get_to_mine phase 2: switch to phase 3(the move up north)");
+      get_to_mine_phase++;
+      set_move_forward_till(true);
+      break;
+    case 3: // move up north
+      if (stopped_turning) {break;}
+      move_forward_till(distance_up_north, 1.0, false); // CHANGE THE FUNCTION TO ACCOUNT FOR BACK SENSOR BEFORE USE
+      if (!move_forward_till_on()) {
+        Serial.println("get_to_mine phase 3 end: finished moving up north");
+        get_to_mine_phase++;
+      }
+      break;
+    case 4: // turn east
+      Serial.println("get_to_mine phase 4: turning east");
+      change_direction(90);
+      get_to_mine_phase++;
+      break;
+    case 5: // approach the mine by the amount we backed up
+      if (!stopped_turning) {break;}
+      Serial.println("get_to_mine phase 5: moving forward towards the mine");
+      move_forward(speed);
+      delay(duration);
+      motor_stop();
+      get_to_mine_phase++;
+      break;
+    case 6:
+      Serial.println("Got to the mine!");
+      stop_get_to_mine();
+      return true;
+  }
+  return false;
 }
